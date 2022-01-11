bc164efd4f82ef09e0b78b7809e81908163166a3
jlbuild <juliabuildbot@gmail.com>
2022-01-11 20:43:51 +0000

    AMReX_jll build 22.1.0+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -3,11 +3,10 @@ export libamrex
 
 using CompilerSupportLibraries_jll
 using MPICH_jll
-using MicrosoftMPI_jll
 JLLWrappers.@generate_wrapper_header("AMReX")
 JLLWrappers.@declare_library_product(libamrex, "@rpath/libamrex.dylib")
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, MPICH_jll, MicrosoftMPI_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, MPICH_jll)
     JLLWrappers.@init_library_product(
         libamrex,
         "lib/libamrex.dylib",
--- src/wrappers/x86_64-w64-mingw32-cxx11.jl
+++ src/wrappers/x86_64-w64-mingw32-cxx11.jl
@@ -2,12 +2,11 @@
 export libamrex
 
 using CompilerSupportLibraries_jll
-using MPICH_jll
 using MicrosoftMPI_jll
 JLLWrappers.@generate_wrapper_header("AMReX")
 JLLWrappers.@declare_library_product(libamrex, "libamrex.dll")
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, MPICH_jll, MicrosoftMPI_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, MicrosoftMPI_jll)
     JLLWrappers.@init_library_product(
         libamrex,
         "bin\\libamrex.dll",
