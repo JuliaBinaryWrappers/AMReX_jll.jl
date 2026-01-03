7352606e0c34c511392190e2097cc59c46adf516
jlbuild <juliabuildbot@gmail.com>
2026-01-03 16:47:46 +0000

    AMReX_jll build 26.1.0+0

--- src/AMReX_jll.jl
+++ src/AMReX_jll.jl
@@ -8,5 +8,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("AMReX")
-JLLWrappers.@generate_main_file("AMReX", UUID("9b6a1cb2-ee7e-5f22-a98b-ab382f85f133"))
+JLLWrappers.@generate_main_file("AMReX", Base.UUID("9b6a1cb2-ee7e-5f22-a98b-ab382f85f133"))
 end  # module AMReX_jll
